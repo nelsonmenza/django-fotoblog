788cca1682fbb3b0cd12f3b3518c8ac5ac4dcf16
Nelson Mendez <105968684+nelsonmenza@users.noreply.github.com>
2023-11-02 11:44:28 -0700

    Added upload multiple photos funcionality

--- blog/urls.py
+++ blog/urls.py
@@ -8,4 +8,9 @@ urlpatterns = [
     path("blog/create", views.blog_and_photo_upload, name="blog_create"),
     path("blog/<int:blog_id>", views.view_blog, name="view_blog"),
     path("blog/<int:blog_id>/edit", views.edit_blog, name="edit_blog"),
+    path(
+        "photo/upload-multiple/",
+        views.create_multiple_photos,
+        name="create_multiple_photos",
+    ),
 ]
